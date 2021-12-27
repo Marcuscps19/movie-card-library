925c03ceccadd68599eb113d5cf81fb4f77047b6
Marcus Hüsemann <mchusemann@gmail.com>
2021-12-27 19:49:21 -0300

    feat(Rating.jsx): adiciona texto IMDb antes da nota

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function Rating({ rating }) {
-  return <span className="rating">{rating}</span>;
+  return <span className="rating">{`IMDb: ${rating}`}</span>;
 }
 
 Rating.propTypes = {
