dfeb95ce3a44ac1752cd9679e481e12abcf7444f
Marcus Hüsemann <mchusemann@gmail.com>
2021-12-28 12:03:35 -0300

    Updates

---
+++ precache-manifest.89cb246ba5f05f0229bf4ff3a49ad929.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b33a26dc074dfb902f6a5d8e89fd17d8",
+    "url": "/movie-card-library/index.html"
+  },
+  {
+    "revision": "50de499a98fa9e09a578",
+    "url": "/movie-card-library/static/css/main.30da831c.chunk.css"
+  },
+  {
+    "revision": "6c25dd8c2233e127eb30",
+    "url": "/movie-card-library/static/js/2.486b3956.chunk.js"
+  },
+  {
+    "revision": "50de499a98fa9e09a578",
+    "url": "/movie-card-library/static/js/main.6cbe8360.chunk.js"
+  },
+  {
+    "revision": "291ec59b72a219c429d6",
+    "url": "/movie-card-library/static/js/runtime-main.442b508f.js"
+  }
+]);
